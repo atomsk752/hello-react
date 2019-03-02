5ae021f2ca733c83ed9ebaa76364a8e81f726429
atomsk752 <bigman01kr@gmail.com>
2019-03-02 20:32:13 +0900

    Chapter 4 END

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -3,32 +3,52 @@ import React, { Component } from 'react';
 class EventPractice extends Component {
 
     state = {
+        username: '',
         message: ''
     }
+
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    handleClick = () => {
+        alert(this.state.username + ' : ' + this.state.message);
+        this.setState({
+            username: '',
+            message: ''
+        });
+    }
+
+    handleKeyPress = (e) => {
+        if(e.key === 'Enter'){
+            this.handleClick();
+        }
+    }
+
     render() {
         return (
             <div>
                 <h1>Event Practice</h1>
+
+                <input
+                    type="text"
+                    name="username"
+                    placeholder="user name"
+                    value={this.state.username}
+                    onChange={this.handleChange}
+                />
                 <input
                     type="text"
                     name="message"
                     placeholder="anything u want"
-                    onChange={
-                        (e) => {
-                            this.setState({
-                                message: e.target.value
-                            })
-                        }
-                    }
+                    value={this.state.message}
+                    onChange={this.handleChange}
+                    onKeyPress={this.handleKeyPress}
                 />
-                <button onClick={
-                    () => {
-                        alert(this.state.message);
-                        this.setState({
-                            message: ''
-                        });
-                    }
-                }>Check</button>
+                <button onClick={this.handleClick}>Check</button>
             </div>
         );
     }
